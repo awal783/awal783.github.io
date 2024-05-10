c472a11428015d8868e8366510537a7bfab98c1b
awal783 <135407123+awal783@users.noreply.github.com>
2024-05-10 14:15:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-function initMap() {
-  var url = new URL("https://goo.gl/maps/3KrsNKwmMWzpmnog6");
-  var params = new URLSearchParams(url.search);
-  var latitude = parseFloat(params.get("q").split(",")[0]);
-  var longitude = parseFloat(params.get("q").split(",")[1]);
-  
-  var myLatLng = { lat: latitude, lng: longitude };
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: myLatLng,
-    zoom: 17
-  });
-
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Lokasi Anda'
-  });
-}
