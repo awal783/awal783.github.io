245f5aeb5577eea4ca270e1568ccbc33e0f519b3
awal783 <135407123+awal783@users.noreply.github.com>
2024-05-21 21:40:29 +0700

    Update and rename index.html to main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int s, luas;
+  
+  cout<<"Menghitung luas persegi"<<endl;
+  
+  cout<<"sisi: ";
+  cin>>s;
+  
+  luas = s * s;
+  
+  cout<<"Hasil: "<<luas;
+  
+  return 0;
+}
+                    
