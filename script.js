181d32bcbfd518177d43c0ee8e1c3a1238e09180
awal783 <135407123+awal783@users.noreply.github.com>
2024-06-03 15:37:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,48 @@
-const baseur1 ="https://rerofya.github.io/resources/"
-const character = [
+const baseUrl = "https://rerofya.github.io/resources/";
+const characters = [
     {
-        name:"Furina",
-        image:"player1.svg"
+        name: "Furina",
+        image: "player1.svg"
     },
     {
-        name:"Raden Ei",
-        image:"player2.svg"
+        name: "Raden Ei",
+        image: "player2.svg"
     },
     {
-        name:"Zoro",
-        image:"player3.svg"
+        name: "Zoro",
+        image: "player3.svg"
     },
     {
-        name:"Luffy",
-        imag:"player4.svg"
+        name: "Luffy",
+        image: "player4.svg"
     }
-]
-let current = 0
-const player = document.querySelector('.player')
-const pName = document.querySelect0r('.name')
-const leftA = document.querySelector('.left')
-const rightA = document.querySelector('.right')
+];
 
- function setplayer() {
-    player.setAttribute('src', baseur1 = character[current].image)
-    pName.textContent = character[current].name
- }
- leftA.addEventListener('click', function() {
+let current = 0;
+const player = document.querySelector('.player');
+const pName = document.querySelector('.name');
+const leftA = document.querySelector('.left');
+const rightA = document.querySelector('.right');
+
+function setPlayer() {
+    player.setAttribute('src', baseUrl + characters[current].image);
+    pName.textContent = characters[current].name;
+}
+
+leftA.addEventListener('click', function() {
     if (current > 0) {
-        current--
-        setplayer()
+        current--;
+        setPlayer();
     }
- })
- rightA.addEventListener('click', function() {
-    if (current + 1 < character.length) {
-        current++
-        setplayer()
+});
+
+rightA.addEventListener('click', function() {
+    if (current + 1 < characters.length) {
+        current++;
+        setPlayer();
     }
- })
+});
+
+// Set player and name initially
+setPlayer();
+
