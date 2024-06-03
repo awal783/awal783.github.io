483abcf80cb8ce364208e7af4767cb94e273cf59
awal783 <135407123+awal783@users.noreply.github.com>
2024-06-03 15:53:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const baseUrl = "https://rerofya.github.io/resources/";
 const characters = [
     {
         name: "Furina",
-        image: "player1.svg"
+        image: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.redbubble.com%2Fi%2Fsticker%2FGenshin-Impact-Furina-Chibi-by-RussellStudio%2F153508169.EJUG5&psig=AOvVaw0h6-a0izGP_MzdS4bGfqPZ&ust=1717490750042000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNC_54mHv4YDFQAAAAAdAAAAABAE"
     },
     {
         name: "Raden Ei",
-        image: "player2.svg"
+        image: "https://dribbble.com/tags/raiden-shogun"
     },
     {
         name: "Zoro",
@@ -32,7 +32,7 @@ function setPlayer() {
 leftA.addEventListener('click', function() {
     if (current > 0) {
         current--;
-        setPlayer();
+        setPlayer():
     }
 });
 
